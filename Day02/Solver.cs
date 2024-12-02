67df42bbba6dc3e2fd12324e78461b548bfe52c8
Adam Monikowski <zjyslav@gmail.com>
2024-12-02 11:28:29 +0100

    Add Day02 Part 1 solution and output

--- Day02/Solver.cs
+++ Day02/Solver.cs
@@ -11,7 +11,41 @@ public class Solver
 
   public int SolvePart1()
   {
-    throw new NotImplementedException();
+    int minChange = 1;
+    int maxChange = 3;
+
+    List<int[]> reports = _input
+      .Select(line => line.Split(' ').Select(x => int.Parse(x)).ToArray())
+      .ToList();
+
+    int safeCount = 0;
+
+    foreach (var report in reports)
+    {
+      bool safe = true;
+      bool increasing = report[1] > report[0];
+
+      for (int i = 1; i < report.Length; i++)
+      {
+        int change = increasing
+          ? report[i] - report[i - 1]
+          : report[i - 1] - report[i];
+
+        if (change < minChange ||
+            change > maxChange)
+        {
+          safe = false;
+          break;
+        }
+      }
+
+      if (safe)
+      {
+        safeCount++;
+      }
+    }
+
+    return safeCount;
   }
 
   public int SolvePart2()
