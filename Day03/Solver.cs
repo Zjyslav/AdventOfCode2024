f55fe685d115cc4ce0b680055e29a9021fe136ff
Adam Monikowski <zjyslav@gmail.com>
2024-12-03 09:26:26 +0100

    Add Day03 Part 1 test

---
+++ Day03/Solver.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode2024.Day03;
+
+public class Solver
+{
+  private string[] _input;
+  public Solver(string[] input)
+  {
+    _input = input;
+  }
+
+  public long SolvePart1()
+  {
+    throw new NotImplementedException();
+  }
+
+  public long SolvePart2()
+  {
+    throw new NotImplementedException();
+  }
+}
--- Tests/ExampleInputTests.cs
+++ Tests/ExampleInputTests.cs
@@ -70,5 +70,20 @@ public class ExampleInputTests
     Assert.Equal(expected, actual);
   }
 
+  [Fact]
+  public void Day03_Part1()
+  {
+    // Arrange
+    string inputPath = Path.Combine("ExampleInputs", "Day03.txt");
+    var input = File.ReadAllLines(inputPath);
+    Day03.Solver solver = new(input);
 
+    int expected = 161;
+
+    // Act
+    var actual = solver.SolvePart1();
+
+    // Assert
+    Assert.Equal(expected, actual);
+  }
 }
