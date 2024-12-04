2b0d61dc86e71e174409237b71c6a7e44b667861
Adam Monikowski <zjyslav@gmail.com>
2024-12-04 09:10:12 +0100

    Fix incorrect day number in Day03 output

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -6,6 +6,6 @@ Solver solver = new(input);
 var part1Output = solver.SolvePart1();
 var part2Output = solver.SolvePart2();
 
-Console.WriteLine("Day 01");
+Console.WriteLine("Day 03");
 Console.WriteLine($"Part 1: {part1Output}");
 Console.WriteLine($"Part 2: {part2Output}");
