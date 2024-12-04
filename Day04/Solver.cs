83eeb746dbac8daf352d313d6a00da19ec005dc2
Adam Monikowski <zjyslav@gmail.com>
2024-12-04 10:02:33 +0100

    Refactor Day04 Part 1 solution

--- Day04/Solver.cs
+++ Day04/Solver.cs
@@ -3,7 +3,6 @@
 public class Solver
 {
   private readonly char[][] _input;
-  private readonly char[] _xmas = "XMAS".ToCharArray();
   public Solver(string[] input)
   {
     _input = input
@@ -16,12 +15,14 @@ public class Solver
     int rowCount = _input.Length;
     int colCount = _input[0].Length;
 
+    char[] xmas = "XMAS".ToCharArray();
+
     int matchCount = 0;
     for (int row = 0; row < rowCount; row++)
     {
       for (int col = 0; col < colCount; col++)
       {
-        matchCount += CountMatches(row, col);
+        matchCount += CountMatches(row, col, xmas);
       }
     }
 
@@ -33,126 +34,165 @@ public class Solver
     throw new NotImplementedException();
   }
 
-  private int CountMatches(int row, int col)
+  private int CountMatches(int row, int col, char[] phrase)
   {
     int matchCount = 0;
     if (row >= _input.Length ||
         col >= _input[row].Length ||
-        _input[row][col] != _xmas[0])
+        _input[row][col] != phrase[0])
     {
       return matchCount;
     }
 
-    if (IsMatchHorizontalForward(row, col)) matchCount++;
-    if (IsMatchHorizontalBackward(row, col)) matchCount++;
-    if (IsMatchVerticalForward(row, col)) matchCount++;
-    if (IsMatchVerticalBackward(row, col)) matchCount++;
-    if (IsMatchDiagonalForwardUp(row, col)) matchCount++;
-    if (IsMatchDiagonalForwardDown(row, col)) matchCount++;
-    if (IsMatchDiagonalBackwardUp(row, col)) matchCount++;
-    if (IsMatchDiagonalBackwardDown(row, col)) matchCount++;
+    if (IsMatchHorizontalForward(row, col, phrase)) matchCount++;
+    if (IsMatchHorizontalBackward(row, col, phrase)) matchCount++;
+    if (IsMatchVerticalForward(row, col, phrase)) matchCount++;
+    if (IsMatchVerticalBackward(row, col, phrase)) matchCount++;
+    if (IsMatchDiagonalForwardUp(row, col, phrase)) matchCount++;
+    if (IsMatchDiagonalForwardDown(row, col, phrase)) matchCount++;
+    if (IsMatchDiagonalBackwardUp(row, col, phrase)) matchCount++;
+    if (IsMatchDiagonalBackwardDown(row, col, phrase)) matchCount++;
 
     return matchCount;
   }
 
-  private bool IsMatchHorizontalForward(int row, int col)
+  private bool IsMatchHorizontalForward(int row, int col, char[] phrase)
   {
-    if (_input[row].Length - col < 4)
+    if (_input[row].Length - col < phrase.Length)
     {
       return false;
     }
 
-    return _input[row][col + 1] == _xmas[1] &&
-           _input[row][col + 2] == _xmas[2] &&
-           _input[row][col + 3] == _xmas[3];
+    for (int i = 1; i < phrase.Length; i++)
+    {
+      if (_input[row][col + i] != phrase[i])
+      {
+        return false;
+      }
+    }
+    return true;
   }
 
-  private bool IsMatchHorizontalBackward(int row, int col)
+  private bool IsMatchHorizontalBackward(int row, int col, char[] phrase)
   {
-    if (col < 3)
+    if (col < phrase.Length - 1)
     {
       return false;
     }
 
-    return _input[row][col - 1] == _xmas[1] &&
-           _input[row][col - 2] == _xmas[2] &&
-           _input[row][col - 3] == _xmas[3];
+    for (int i = 1; i < phrase.Length; i++)
+    {
+      if (_input[row][col - i] != phrase[i])
+      {
+        return false;
+      }
+    }
+    return true;
   }
 
-  private bool IsMatchVerticalForward(int row, int col)
+  private bool IsMatchVerticalForward(int row, int col, char[] phrase)
   {
-    if (_input.Length - row < 4)
+    if (_input.Length - row < phrase.Length)
     {
       return false;
     }
 
-    return _input[row + 1][col] == _xmas[1] &&
-           _input[row + 2][col] == _xmas[2] &&
-           _input[row + 3][col] == _xmas[3];
+    for (int i = 1; i < phrase.Length; i++)
+    {
+      if (_input[row + i][col] != phrase[i])
+      {
+        return false;
+      }
+    }
+    return true;
   }
 
-  private bool IsMatchVerticalBackward(int row, int col)
+  private bool IsMatchVerticalBackward(int row, int col, char[] phrase)
   {
-    if (row < 3)
+    if (row < phrase.Length - 1)
     {
       return false;
     }
 
-    return _input[row - 1][col] == _xmas[1] &&
-           _input[row - 2][col] == _xmas[2] &&
-           _input[row - 3][col] == _xmas[3];
+    for (int i = 1; i < phrase.Length; i++)
+    {
+      if (_input[row - i][col] != phrase[i])
+      {
+        return false;
+      }
+    }
+    return true;
   }
 
-  private bool IsMatchDiagonalForwardUp(int row, int col)
+  private bool IsMatchDiagonalForwardUp(int row, int col, char[] phrase)
   {
-    if (row < 3 ||
-        _input[row].Length - col < 4)
+    if (row < phrase.Length - 1 ||
+        _input[row].Length - col < phrase.Length)
     {
       return false;
     }
 
-    return _input[row - 1][col + 1] == _xmas[1] &&
-           _input[row - 2][col + 2] == _xmas[2] &&
-           _input[row - 3][col + 3] == _xmas[3];
+    for (int i = 1; i < phrase.Length; i++)
+    {
+      if (_input[row - i][col + i] != phrase[i])
+      {
+        return false;
+      }
+    }
+    return true;
   }
 
-  private bool IsMatchDiagonalForwardDown(int row, int col)
+  private bool IsMatchDiagonalForwardDown(int row, int col, char[] phrase)
   {
-    if (_input.Length - row < 4 ||
-        _input[row].Length - col < 4)
+    if (_input.Length - row < phrase.Length ||
+        _input[row].Length - col < phrase.Length)
     {
       return false;
     }
 
-    return _input[row + 1][col + 1] == _xmas[1] &&
-           _input[row + 2][col + 2] == _xmas[2] &&
-           _input[row + 3][col + 3] == _xmas[3];
+    for (int i = 1; i < phrase.Length; i++)
+    {
+      if (_input[row + i][col + i] != phrase[i])
+      {
+        return false;
+      }
+    }
+    return true;
   }
 
-
-  private bool IsMatchDiagonalBackwardUp(int row, int col)
+  private bool IsMatchDiagonalBackwardUp(int row, int col, char[] phrase)
   {
-    if (row < 3 ||
-        col < 3)
+    if (row < phrase.Length - 1 ||
+        col < phrase.Length - 1)
     {
       return false;
     }
 
-    return _input[row - 1][col - 1] == _xmas[1] &&
-           _input[row - 2][col - 2] == _xmas[2] &&
-           _input[row - 3][col - 3] == _xmas[3];
+    for (int i = 1; i < phrase.Length; i++)
+    {
+      if (_input[row - i][col - i] != phrase[i])
+      {
+        return false;
+      }
+    }
+    return true;
   }
 
-  private bool IsMatchDiagonalBackwardDown(int row, int col)
+  private bool IsMatchDiagonalBackwardDown(int row, int col, char[] phrase)
   {
-    if (_input.Length - row < 4 ||
-        col < 3)
+    if (_input.Length - row < phrase.Length ||
+        col < phrase.Length - 1)
     {
       return false;
     }
 
-    return _input[row + 1][col - 1] == _xmas[1] &&
-           _input[row + 2][col - 2] == _xmas[2] &&
-           _input[row + 3][col - 3] == _xmas[3];
+    for (int i = 1; i < phrase.Length; i++)
+    {
+      if (_input[row + i][col - i] != phrase[i])
+      {
+        return false;
+      }
+    }
+    return true;
   }
 }
