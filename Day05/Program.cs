a8fc9698ba64c7c66a518f9a7042df0fc843bf9e
Adam Monikowski <zjyslav@gmail.com>
2024-12-05 08:51:05 +0100

    Add Day05 Part 1 solution and output

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdventOfCode2024.Day05;
+
+var input = File.ReadAllLines("input.txt");
+Solver solver = new(input);
+
+var part1Output = solver.SolvePart1();
+//var part2Output = solver.SolvePart2();
+
+Console.WriteLine("Day 05");
+Console.WriteLine($"Part 1: {part1Output}");
+//Console.WriteLine($"Part 2: {part2Output}");
--- Day05/Solver.cs
+++ Day05/Solver.cs
@@ -10,7 +10,72 @@ public class Solver
 
   public int SolvePart1()
   {
-    throw new NotImplementedException();
+    Dictionary<int, List<int>> orderingRules = [];
+    List<int[]> updates = [];
+
+    int inputBreakIndex = 0;
+
+    for (int i = 0; i < _input.Length; i++)
+    {
+      if (_input[i] == string.Empty)
+      {
+        inputBreakIndex = i;
+        break;
+      }
+
+      var pages = _input[i]
+        .Split('|')
+        .Select(s => int.Parse(s))
+        .ToArray();
+
+      if (!orderingRules.ContainsKey(pages[0]))
+      {
+        orderingRules[pages[0]] = [];
+      }
+      orderingRules[pages[0]].Add(pages[1]);
+    }
+
+    for (int i = inputBreakIndex + 1; i < _input.Length; i++)
+    {
+      var update = _input[i]
+        .Split(',')
+        .Select(s => int.Parse(s))
+        .ToArray();
+
+      updates.Add(update);
+    }
+
+    int sum = 0;
+
+    foreach (var update in updates)
+    {
+      bool orderIsCorrect = true;
+
+      for (int i = update.Length - 1; i > 0; i--)
+      {
+        var rules = orderingRules.GetValueOrDefault(update[i]);
+        if (rules is null)
+        {
+          continue;
+        }
+
+        for (int j = 0; j < i; j++)
+        {
+          if (rules.Contains(update[j]))
+          {
+            orderIsCorrect = false;
+          }
+        }
+      }
+
+      if (orderIsCorrect)
+      {
+        int middleIndex = update.Length / 2;
+        sum += update[middleIndex];
+      }
+    }
+
+    return sum;
   }
 
   public int SolvePart2()
